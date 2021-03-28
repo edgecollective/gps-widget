189c9c3720667c3e7b3ae44fb9579ce8e900248c
Don Blair <donblair@gmail.com>
2021-03-28 12:19:03 -0400

    adding working proto

---
+++ v_0.1/mighty_eink_roamah_sd_sleep_lora/mighty_eink_roamah_sd_sleep_lora.ino
@@ -0,0 +1,337 @@
+/***************************************************
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+#include <LowPower.h>
+#include "Adafruit_EPD.h"
+#define EPD_CS     22
+#define EPD_DC     21
+#define SRAM_CS     20
+#define EPD_RESET   19 // can set to -1 and share with microcontroller Reset!
+#define EPD_BUSY    18 // can set to -1 to not use a pin (will wait a fixed delay)
+
+Adafruit_IL0373 display(152, 152, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+
+#define COLOR1 EPD_BLACK
+#define COLOR2 EPD_RED
+
+// lora
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 7
+#define RFM95_INT 2 
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// ss
+#include <SoftwareSerial.h>
+SoftwareSerial ss(14, 15); //rx,tx
+
+// gps
+#include <TinyGPS.h>
+TinyGPS gps;
+
+//sd
+// include the SD library:
+#include <SD.h>
+#include <SPI.h>
+
+const int chipSelect = 4;
+
+// power circuitry
+int enablePin = 23;
+
+
+int x = 10; // x position of the marker
+int y = 10; // y "
+ 
+int max_x = 150; // width of the gridsquare in pixels on the display
+int max_y = 100; // height "
+
+float xfrac=0; // fractional position (of max_x) of the marker on the gridsquare
+float yfrac=0; // " (of max_y) "
+
+char locator[9]; // gridsquare locator
+
+float unit = 0.004166666666666667;
+
+char * getLocator(float lat, float lon, int precision) {
+  
+  float ydiv_arr[] = {10,1,0.04166666666,0.00416666666,0.00017361111};
+  char d1[]="ABCDEFGHIJKLMNOPQR";
+  char d2[]="ABCDEFGHIJKLMNOPQRSTUVWX";
+
+  int this_precision = 4;
+
+  strcpy(locator,"");
+  
+  float q = lon;
+  float p = lat;
+  float rlon,rlat;
+  char cstr[]="1";
+  int num;
+  
+  while (q < -180) {q += 360;}
+      while (q > 180) {q -=360;}
+      q = q + 180;
+      p = p + 90;
+      
+      strncat(locator,&d1[(int) floor(q/20)],1);
+      
+      strncat(locator, &d1[(int) floor(p/10)],1);
+  
+   
+      for (int i=0; i<4; i=i+1) {
+    if (this_precision > i+1) {
+        rlon = fmod(q,ydiv_arr[i]*2);
+        rlat = fmod(p,ydiv_arr[i]);
+      if ((i%2)==0) {
+        num = floor(rlon/(ydiv_arr[i+1]*2));
+        itoa(num, cstr, 10);
+
+        strcat(locator,cstr);
+        strcat(locator,"");
+        num = floor(rlat/(ydiv_arr[i+1]));
+        itoa(num, cstr, 10);
+        strcat(locator,cstr);
+
+      } else {
+
+        strncat(locator,&d2[(int) floor(rlon/(ydiv_arr[i+1]*2))],1);
+        strcat(locator,"");
+        strncat(locator,&d2[(int) floor(rlat/(ydiv_arr[i+1]))],1);
+      }
+    }
+    }  
+    return (locator);
+}
+
+void log_data(char locator[]) {
+File dataFile = SD.open("datalog.txt", FILE_WRITE);
+   if (dataFile) {
+    dataFile.println(locator);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(locator);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
+void display_map(float lat, float lon, uint8_t max_x, uint8_t max_y, uint8_t x, uint8_t y, char locator[]) {
+
+  display.clearBuffer();
+
+  //big frame
+  display.drawLine(0,0,max_x,0,COLOR1);
+  display.drawLine(0,max_y,max_x,max_y,COLOR1);
+
+  //minor frame
+  display.drawLine(0,round(max_y/2),max_x,round(max_y/2),COLOR2);
+  display.drawLine(round(max_x/2),0,round(max_x/2),max_y,COLOR2);
+
+
+  display.drawCircle(x, y, 6, COLOR1);
+  display.fillCircle(x, y, 3, COLOR2);
+
+  // info display
+  //display.drawLine(90,max_y,90,150,COLOR2);
+
+  int current_y = max_y;
+  int step_y = 10;
+  int current_x = 5;
+
+  display.setTextWrap(false);
+
+  display.setTextSize(2);
+  current_y=current_y+5;
+  display.setCursor(current_x, current_y);
+  display.setTextColor(COLOR2);
+  //display.print(" GS: ");
+  display.print(locator);
+
+  display.setTextSize(1);
+  current_y=current_y+step_y*2;
+  display.setCursor(current_x,current_y);
+  display.setTextColor(COLOR1);
+  display.print("lat: ");
+  display.print(lat);
+  
+  current_y=current_y+step_y;
+  display.setCursor(current_x,current_y);
+  display.setTextColor(COLOR1);
+  display.print("lon: ");
+  display.print(lon);
+  
+
+
+  
+  
+  display.display();
+
+  /*
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, COLOR1);
+  }
+
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, COLOR2);  // on grayscale this will be mid-gray
+  }
+  */
+  
+}
+
+
+void setup() {
+
+  pinMode(enablePin,OUTPUT);
+  digitalWrite(enablePin, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);
+  
+  Serial.begin(115200);
+  //while (!Serial) { delay(10); }
+  Serial.println("Start");
+
+  ss.begin(9600);
+  
+  // lora
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  
+}
+
+void loop() {
+
+  digitalWrite(enablePin, HIGH);
+  
+  
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (ss.available())
+    {
+      char c = ss.read();
+      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+  
+ if (newData)
+  {
+
+  float flat, flon;
+  unsigned long age;
+  gps.f_get_position(&flat, &flon, &age);
+  
+  //flat =44.42849861396812;
+  //flon =-69.00385950557286;
+  
+  float myLat = flat;
+  if (myLat > 85) myLat = 85;
+  if (myLat < -85) myLat = -85;
+
+  //find lat/lon bounds of the gridsquare
+  float lon_left = floor(flon/(unit*2))*(unit*2);
+  float lon_right = ceil(flon/(unit*2))*(unit*2);
+  float lat_top = ceil(myLat/unit)*unit;
+  float lat_bottom=floor(myLat/unit)*unit;
+
+  float xfrac = (flon-lon_left)/(lon_right-lon_left);
+  
+  float yfrac = 1-(flat-lat_bottom)/(lat_top-lat_bottom);
+
+  x=round(xfrac*max_x);
+  y=round(yfrac*max_y);
+    
+  Serial.println(getLocator(flat,flon, 4));
+
+
+  display.begin();
+
+  // large block of text
+  //display.clearBuffer();
+  //testdrawtext("New Loop", COLOR1);
+  //display.display();
+
+  //delay(5000);
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+
+  
+  display_map(flat,flon,max_x,max_y,x,y,getLocator(flat,flon, 4));
+  
+  log_data(getLocator(flat,flon, 4));
+
+  digitalWrite(enablePin, LOW);
+  //delay(10000);
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); 
+  
+}
+
+  gps.stats(&chars, &sentences, &failed);
+  
+  if (chars == 0)
+    Serial.println("** No characters received from GPS: check wiring **");
+    
+}
+
+
+void testdrawtext(char *text, uint16_t color) {
+  display.setCursor(0, 0);
+  display.setTextColor(color);
+  display.setTextWrap(true);
+  display.print(text);
+}
---
+++ v_0.1/mighty_eink_transmit_sd_exp1B/mighty_eink_transmit_sd_exp1B.ino
@@ -0,0 +1,228 @@
+// Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
+
+// enable
+
+#define ENABLE A5
+
+//sd
+// include the SD library:
+#include <SD.h>
+#include <SPI.h>
+
+const int chipSelect = 4;
+
+/// eink
+
+#include "Adafruit_EPD.h"
+
+#define EPD_CS     18
+#define EPD_DC     19
+#define SRAM_CS     20
+#define EPD_RESET   21 // can set to -1 and share with microcontroller Reset!
+#define EPD_BUSY    22 // can set to -1 to not use a pin (will wait a fixed delay)
+
+Adafruit_IL0373 display(152, 152, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+
+#define COLOR1 EPD_BLACK
+#define COLOR2 EPD_RED
+
+
+/// lora
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+
+#define RFM95_CS 22 
+#define RFM95_RST 23
+#define RFM95_INT 11 
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+
+
+void eink_setup() {
+  display.begin();
+  Serial.println("eink setup.");
+}
+void eink_write(char *text) {
+  /// display
+  Serial.println("eink test");
+  //
+  // large block of text
+  display.clearBuffer();
+  testdrawtext(text, COLOR1);
+  display.display();
+}
+
+
+void sd_setup() {
+    //sd card
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+
+void log_data(char *text) {
+File dataFile = SD.open("datalog.txt", FILE_WRITE);
+   if (dataFile) {
+    dataFile.println(text);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(text);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
+
+void lora_setup() {
+
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  
+}
+
+void lora_ping() {
+  
+   char radiopacket[20] = "Hello World #      ";
+  itoa(packetnum++, radiopacket+13, 10);
+  
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+  
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+  
+  Serial.println("Sending...");
+  delay(10);
+  
+  rf95.send((uint8_t *)radiopacket, 20);
+
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  digitalWrite(LED, HIGH);
+  rf95.waitPacketSent();
+  digitalWrite(LED, LOW);
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+}
+
+
+void testdrawtext(char *text, uint16_t color) {
+  display.setCursor(0, 0);
+  display.setTextColor(color);
+  display.setTextWrap(true);
+  display.print(text);
+}
+
+
+void setup() 
+{
+
+  Serial.begin(9600);
+  Serial.println("hello");
+
+//  pinMode(ENABLE, OUTPUT);
+//  digitalWrite(ENABLE, LOW);
+//  Serial.println("ENABLE LOW");
+//  delay(3000);
+
+  
+  
+  sd_setup();
+  lora_setup();
+  //eink_setup();
+  
+}
+
+int index = 0;
+
+void loop()
+{
+
+  
+  
+  log_data("hello");
+  //eink_setup();
+  //eink_write("hello");
+  //lora_setup();
+  lora_ping();
+  delay(2000);
+
+  
+  
+}
