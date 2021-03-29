e7aeefc0e16e2576fcad7f0ead9bef98eccebb3b
Don Blair <donblair@gmail.com>
2021-03-29 09:36:39 -0400

    mighty_eink_transmit_sd_gps_sleep_exp1C_working sleeps at 0.1 mA

---
+++ v_0.1/mighty_eink_transmit_sd_gps_sleep_exp1C_working/mighty_eink_transmit_sd_gps_sleep_exp1C_working.ino
@@ -0,0 +1,307 @@
+// Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
+
+
+// LED
+#define myLED A7
+
+// sleep
+#include <LowPower.h>
+
+// ss
+#include <SoftwareSerial.h>
+SoftwareSerial ss(12, 14); //rx,tx
+
+// gps
+#include <TinyGPS.h>
+TinyGPS gps;
+
+// enable
+#define ENABLE A0
+
+//sd
+// include the SD library:
+
+#include <SD.h>
+#include <SPI.h>
+
+const int chipSelect = 4;
+
+/// eink
+
+#include "Adafruit_EPD.h"
+
+#define EPD_CS     18
+#define EPD_DC     19
+#define SRAM_CS     20
+#define EPD_RESET   21 // can set to -1 and share with microcontroller Reset!
+#define EPD_BUSY    15 // can set to -1 to not use a pin (will wait a fixed delay)
+
+Adafruit_IL0373 display(152, 152, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+
+#define COLOR1 EPD_BLACK
+#define COLOR2 EPD_RED
+
+
+/// lora
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+
+#define RFM95_CS 22 
+#define RFM95_RST 23
+#define RFM95_INT 11 
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+
+
+void eink_setup() {
+  display.begin();
+  Serial.println("eink setup.");
+}
+void eink_write(char *text) {
+  /// display
+  Serial.println("eink test");
+  //
+  // large block of text
+  display.clearBuffer();
+  testdrawtext(text, COLOR1);
+  display.display();
+}
+
+
+void sd_setup() {
+    //sd card
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+
+void log_data(char *text) {
+File dataFile = SD.open("datalog.txt", FILE_WRITE);
+   if (dataFile) {
+    dataFile.println(text);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(text);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
+
+void lora_setup() {
+
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  
+}
+
+void lora_ping() {
+  
+   char radiopacket[20] = "Hello World #      ";
+  itoa(packetnum++, radiopacket+13, 10);
+  
+  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
+  Serial.println("Transmitting..."); // Send a message to rf95_server
+  
+  Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+  
+  Serial.println("Sending...");
+  delay(10);
+  
+  rf95.send((uint8_t *)radiopacket, 20);
+
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  digitalWrite(LED, HIGH);
+  rf95.waitPacketSent();
+  digitalWrite(LED, LOW);
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+
+  rf95.sleep();
+}
+
+
+void testdrawtext(char *text, uint16_t color) {
+  display.setCursor(0, 0);
+  display.setTextColor(color);
+  display.setTextWrap(true);
+  display.print(text);
+}
+
+
+void setup() 
+{
+
+  pinMode(ENABLE, OUTPUT);
+  digitalWrite(ENABLE, LOW);
+  
+  pinMode(myLED, OUTPUT);
+
+  for (int i = 0; i<2; i++) {
+    digitalWrite(myLED,HIGH);
+    delay(100);
+    digitalWrite(myLED,LOW);
+    delay(100);
+  }
+  
+  Serial.begin(9600);
+  Serial.println("starting up ----------------");
+
+  delay(1000);
+  
+  
+  
+  ss.begin(9600);
+  
+//  delay(3000);
+
+  
+  // order 1
+  //sd_setup();
+  //lora_setup();
+  //eink_setup();
+
+  
+}
+
+int index = 0;
+
+void loop()
+{
+
+  digitalWrite(ENABLE, HIGH);
+  Serial.println("ENABLE HIGH");
+
+
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (ss.available())
+    {
+      char c = ss.read();
+      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+
+  if (newData)
+  {
+    
+  float flat, flon;
+  unsigned long age;
+  gps.f_get_position(&flat, &flon, &age);
+  Serial.println(flat);
+  
+  sd_setup();
+  log_data("hello");
+  delay(1000);
+  
+  eink_setup();
+  eink_write("hello");
+  delay(1000);
+  
+  lora_setup();
+  lora_ping();
+  delay(1000);
+  
+  digitalWrite(ENABLE, LOW);
+  Serial.println("ENABLE LOW");
+
+  delay(1000);
+  
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  //delay(3000);
+  
+  }
+
+  gps.stats(&chars, &sentences, &failed);
+  
+  if (chars == 0)
+    Serial.println("** No characters received from GPS: check wiring **");
+    
+
+}
